126ca715deb537e0d8d434e3cf6e740ddfc1a806
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-09 07:11:49 -0500

    Add kelvin function and display to html.

--- js/main.js
+++ js/main.js
@@ -13,19 +13,25 @@ function celsius () {
   
   let farenComputed = (9/5)*inputValue + 32;
   let kelComputed = inputValue + 273.15;
-  console.log(farenComputed);
-  console.log(kelComputed);
+
+  document.getElementById('seeFirst').innerHTML = `Celsius to Fahrenheit is ${farenComputed}`;
+  document.getElementById('seeSecond').innerHTML =`Celsius to Fahrenheit is ${kelComputed}`;
 }
 
 function fahrenheit () {
   const inputValue = Number(document.getElementById('tvalue').value);
   let celComputed = (5/9)*(inputValue-32);
   let kelComputed = (5/9)*(inputValue+459.67);
-  console.log(celComputed);
-  console.log(kelComputed);
+
+  document.getElementById('seeFirst').innerHTML = `Fahrenheit to Celsius is ${celComputed}`;
+  document.getElementById('seeSecond').innerHTML =`Fahrenheit to Kelvin is ${kelComputed}`;
 }
 
 function kelvin () {
   const inputValue = Number(document.getElementById('tvalue').value);
+  let celComputed = inputValue - 273.15;
+  let farenComputed = 9/5*(inputValue - 273) + 32;
 
+  document.getElementById('seeFirst').innerHTML = `Kelvin to Celsius is ${celComputed}`;
+  document.getElementById('seeSecond').innerHTML =`Kelvin to Fahrenheit is ${farenComputed}`;
 } 
