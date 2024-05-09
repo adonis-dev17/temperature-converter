e8769fae2707afbed79f5545a24fa063779e7ac0
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-09 19:01:19 +0800

    Change names in html and add formula for computing the celsius and fahrenheit functions.

--- js/main.js
+++ js/main.js
@@ -2,18 +2,30 @@
 
 // will accept values 
 
+
 document.querySelector('#Celsius').addEventListener('click', celsius);
 document.querySelector('#Fahrenheit').addEventListener('click', fahrenheit);
 document.querySelector('#Kelvin').addEventListener('click', kelvin);
 
+
 function celsius () {
-  alert('Celsius!');
+  const inputValue = Number(document.getElementById('tvalue').value);
+  
+  let farenComputed = (9/5)*inputValue + 32;
+  let kelComputed = inputValue + 273.15;
+  console.log(farenComputed);
+  console.log(kelComputed);
 }
 
 function fahrenheit () {
-  alert('Fahrenheit!');
+  const inputValue = Number(document.getElementById('tvalue').value);
+  let celComputed = (5/9)*(inputValue-32);
+  let kelComputed = (5/9)*(inputValue+459.67);
+  console.log(celComputed);
+  console.log(kelComputed);
 }
 
 function kelvin () {
-  alert('Kelvin!');
-}
+  const inputValue = Number(document.getElementById('tvalue').value);
+
+} 
