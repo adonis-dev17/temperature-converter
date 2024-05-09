fb54a725e68f18bd6d612b9e7e3ebf2536549755
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-09 17:42:25 +0800

    Update html forms and add eventlisteners and forms in each temperature type.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 //Write your pseduo code first! 
+
+// will accept values 
+
+document.querySelector('#Celsius').addEventListener('click', celsius);
+document.querySelector('#Fahrenheit').addEventListener('click', fahrenheit);
+document.querySelector('#Kelvin').addEventListener('click', kelvin);
+
+function celsius () {
+  alert('Celsius!');
+}
+
+function fahrenheit () {
+  alert('Fahrenheit!');
+}
+
+function kelvin () {
+  alert('Kelvin!');
+}
